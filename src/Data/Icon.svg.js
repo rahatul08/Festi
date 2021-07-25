4a882d438120bb05319150ce44d91a50ffb046d7
Rahatul Islam Rasel <61477379+rahatul08@users.noreply.github.com>
2021-07-26 00:25:30 +0600

    header

--- src/Data/Icon.svg.js
+++ src/Data/Icon.svg.js
@@ -4,12 +4,16 @@ import user from '../assets/icon/user.svg'
 import carts from '../assets/icon/carts.svg'
 import trolley from '../assets/icon/trolley.svg'
 import like from '../assets/icon/like.svg'
+import heart from '../assets/icon/heart.svg'
+import shopping from '../assets/icon/shopping-cart.svg'
 const Icon_svg ={
     call:call,
     email:email,
     user:user,
     carts:carts,
     trolley:trolley,
-    like:like
+    like:like,
+    heart:heart,
+    shopping:shopping
 }
 export default  Icon_svg;
